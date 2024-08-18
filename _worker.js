6704d1e17c6830d52c2ba3d0bf502d3663fcf58b
lee1080 <38096314+lee1080@users.noreply.github.com>
2024-08-18 11:20:49 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -11,7 +11,6 @@ let addresses = [
 
 // 设置优选地址api接口
 let addressesapi = [
-	'https://home.lee1080.cn/rules/best_cdn'
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
 ];
